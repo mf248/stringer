4d035e7b88967c1eb408e0395fa8a750c378897d
Michael Fergus <mfergus48@gmail.com>
2012-11-26 11:56:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stringer::Application.config.secret_token = '8f752a401bb34e49f8c83dae6a621bc06fe06b914bb1f059e956582b85db1d81c2be9cc29229cd670647690edff6ebdf29c5ea8f0f59c7fb881b39ade1f1db9c'
