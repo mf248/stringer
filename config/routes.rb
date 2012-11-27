6fcc9ea82545b9d2193d8aa19de5620819a503c4
Michael Fergus <mfergus48@gmail.com>
2012-11-26 23:11:12 -0600

    modified the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Stringer::Application.routes.draw do
   resources :users
 
-   root to: 'static_pages#home'
+  match '/index' to: 'static_pages#home'
+
+  match '/home' to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
 
